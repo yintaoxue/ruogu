49af3a2143642f650ed840017a7168bc6fe60850
Talin <yintaoxue@gmail.com>
2016-08-05 21:51:25 +0800

    article kafka

---
+++ project/ruogu/kafka-client/src/main/java/org/ruogu/kafka/client/MyParitioner.java
@@ -0,0 +1,16 @@
+package org.ruogu.kafka.client;
+
+import kafka.producer.Partitioner;
+
+/**
+ * MyParitioner
+ * 
+ * @author xueyintao 2016年8月4日
+ */
+public class MyParitioner implements Partitioner {
+
+	public int partition(Object key, int numPartitions) {
+		return Math.abs(key.hashCode() % numPartitions);
+	}
+
+}
---
+++ project/ruogu/kafka-client/src/main/java/org/ruogu/kafka/client/Producer.java
@@ -0,0 +1,44 @@
+package org.ruogu.kafka.client;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+/**
+ * Producer
+ * 
+ * @author xueyintao 2016年8月4日
+ */
+public class Producer {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("acks", "all");
+		props.put("retries", 0);
+		props.put("batch.size", 16384);
+		props.put("linger.ms", 1);
+		props.put("buffer.memory", 33554432);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("partitioner.class", "org.ruogu.MyParitioner");
+
+
+		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+		for (int i = 0; i < 10; i++) {
+			producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), "msg-" + i));
+			System.out.println("msg-" + i);
+		}
+
+		Thread.sleep(10000L);
+
+		producer.close();
+	}
+
+}
